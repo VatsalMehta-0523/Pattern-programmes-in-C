c4898ec9cca4f6dd9766d910f3b026d54f01dc71
VatsalMehta-0523 <168580559+VatsalMehta-0523@users.noreply.github.com>
2024-05-27 23:13:59 +0530

    Add files via upload

---
+++ Full Pyramid.c
@@ -0,0 +1,33 @@
+/*
+
+output for n = 5
+
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * * 
+
+*/
+
+
+
+#include <stdio.h>
+
+int main()
+{
+  int n;
+  printf("enter number of rows : ");
+  scanf("%d",&n);
+  
+  for( int i = 1 ; i <=n ; i++ ){
+    for ( int space = 1 ; space <= (n-i) ; space++ ){
+      printf(" ");
+    }
+    for ( int j = 1 ; j <= i ; j++){
+      printf("* ");
+    }
+    printf("\n");
+  }
+  return 0;
+}
