6532362cd36e38ba6b678fb50a5b3985f07b7edc
VatsalMehta-0523 <168580559+VatsalMehta-0523@users.noreply.github.com>
2024-05-29 23:09:50 +0530

    Add files via upload

---
+++ inverted left half pyramid.c
@@ -0,0 +1,33 @@
+/*
+
+output pattern for n = 5
+
+ *****
+  ****
+   ***
+    **
+     *
+  
+*/
+
+
+
+#include <stdio.h>
+
+int main()
+{
+  int n;
+  printf("enter number of rows : ");
+  scanf("%d",&n);
+  
+  for( int i = 1 ; i <=n ; i++ ){
+    for (int k = 1 ; k <= i ; k++ ){
+      printf(" ");
+    }
+    for ( int j = n ; j >= i ; j--){
+      printf("*");
+    }
+    printf("\n");
+  }
+  return 0;
+}
