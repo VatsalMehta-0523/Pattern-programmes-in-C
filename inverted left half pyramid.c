308ba2cbbf21564af9e39088343084b91b14b8bd
VatsalMehta-0523 <168580559+VatsalMehta-0523@users.noreply.github.com>
2024-06-04 21:08:13 +0530

    Inverted left half pyramid.c

--- inverted left half pyramid.c
+++ inverted left half pyramid.c
@@ -16,16 +16,16 @@ output pattern for n = 5
 
 int main()
 {
-  int n;
+  int n;                                      // n is the variable to store number of rows 
   printf("enter number of rows : ");
   scanf("%d",&n);
   
   for( int i = 1 ; i <=n ; i++ ){
     for (int k = 1 ; k <= i ; k++ ){
-      printf(" ");
+      printf(" ");                          // this will print " " before printing "*"
     }
     for ( int j = n ; j >= i ; j--){
-      printf("*");
+      printf("*");                          // this will print " * " inrequired position
     }
     printf("\n");
   }
