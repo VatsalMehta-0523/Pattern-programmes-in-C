18124f1d612f9034f9d7bde1a8811cdce492e6e9
VatsalMehta-0523 <168580559+VatsalMehta-0523@users.noreply.github.com>
2024-06-06 22:46:17 +0530

    Add files via upload

---
+++ hollow rectangular shape.c
@@ -0,0 +1,37 @@
+/*
+output -->
+
+enter number of rows : 5
+enter number of columns : 10
+
+* * * * * * * * * * 
+*                 * 
+*                 * 
+*                 * 
+* * * * * * * * * * 
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+  int n , m ;
+  printf("enter number of rows : ");
+  scanf("%d",&n);
+  printf("enter number of columns : ");
+  scanf("%d",&m);
+  printf("\n");
+  for (int i = 1 ; i<=n ; i++ ){
+    for (int j = 1 ; j <= m ; j++ ){
+      if (i ==1|| i == n || j==1 || j==m ){
+        printf("* ");
+      }
+      else{
+        printf("  ");
+      }
+    }
+    printf("\n");
+  }
+  return 0;
+}
